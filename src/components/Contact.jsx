37fe173565eb3c6c63c624758fe9670e9e172866
Zeeshan <mzeeshi187@gmail.com>
2024-12-19 21:46:44 +0500

    changes added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,26 +1,31 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 
 const Contact = () => {
   let [data, setData]=useState(10)
   let [value, setValue]=useState(100)
 
-  useEffect(()=>{
-    console.log("API CAllING");
-  },[value,data])
+  // useEffect(()=>{
+  //   console.log("API CAllING");
+  // },[data])
+  let memoizedfunction = useMemo(()=>{
+    console.log('mesasge inside function');
+    return data*3
+  }, [data])
+    
+ 
   
   return (
     <div>
       <h2>Data:{data}</h2>
       <h2>Value:{value}</h2>
+      <h2>function value:{memoizedfunction}</h2>
       <button onClick={()=>setData(data+1)}>Update Data</button>
       <button onClick={()=>setValue(value+1)}>Update Value</button>
-
     </div>
   )
 }
 
 export default Contact
-
 
 
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,6 @@ import General from './General'
 const Home = () => {
   return (
     <div>
-      
       <General heading="Home Page" para="Home Page Paragraph Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore possimus aspernatur, itaque quos ducimus sequi ipsa quaerat distinctio laborum at mollitia pariatur reprehenderit iusto consectetur numquam debitis tempora voluptas enim." btn="Go to About Page" img="/images/home.png" link="/about" />
     </div>
   )
